8bd08ec1b968fdfbb5a9e1b7cf6328b58008a096
Sebastian5 <juan_srian@soy.sena.edu.co>
2024-08-22 09:43:36 -0500

    Primer commit con archivos para la nube

---
+++ Pruebas/Ciclos/codigoPrueba.py
@@ -0,0 +1,5 @@
+#Esto es un ciclo while
+opcion =0
+while opcion==0:
+    opcion=input('¿quieres salir?\n(0 = no, otro = sí): ')
+print('Salió')
---
+++ Pruebas/Condiciones/codigoPrueba.py
@@ -0,0 +1,6 @@
+#Esto es una condicion
+edad=10
+if (edad >= 18):
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
